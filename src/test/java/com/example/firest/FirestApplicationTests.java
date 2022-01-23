21afe23cc3bd039577ae5b3dfcb37baa1ee9526d
jayavant <jayavantdraut@gmail.com>
2022-01-23 13:21:22 +0530

    intial comit

---
+++ src/test/java/com/example/firest/FirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
